d574ab72d431f48ab85a76a682bd1cd90a077fd8
Patrick Latimer <patrick.latimer@alleninstitute.org>
2025-11-13 16:43:29 -0800

    Update adapter example

--- examples/rpc/adapter/instrument_utils.py
+++ examples/rpc/adapter/instrument_utils.py
@@ -1,24 +1,44 @@
 from functools import partial
-from typing import Type, TypeVar
+import subprocess
+from typing import Optional, Type, TypeVar
 
 from one_liner.client import RouterClient
 
 T = TypeVar("API")
 
 
-def get_connector(protocol: Type[T], remote_device_name: str) -> T:
-    """Create a proxy instance that implements the given protocol"""
-    client = RouterClient()
+def get_connector(
+    protocol: Type[T],
+    port: str = "5555",
+    target_obj_name: str = "adapter",
+    cmd_to_start_adapter_server: Optional[list[str]] = None,
+) -> T:
+    """Create a proxy instance that implements the given protocol
 
-    funcs = protocol.__protocol_attrs__
+    :param protocol: Protocol class defining the API to connect to.
+    :param target_obj_name: Name of the target object in the adapter server.
+    :param port: Port where the adapter server is listening for RPC calls.
+    :param cmd_to_start_adapter_server: If provided, will be run in a subprocess.
+    """
 
-    def func(funcname: str, *args, **kwargs):
-        return client.call(remote_device_name, funcname, args, kwargs)[1]
+    if cmd_to_start_adapter_server:
+        adapter_process = subprocess.Popen(cmd_to_start_adapter_server)
+    else:
+        adapter_process = None
 
+    client = RouterClient(rpc_port=port)
+
+    api_funcs = protocol.__protocol_attrs__
+
+    def call_remote(funcname: str, *args, **kwargs):
+        return client.call(target_obj_name, funcname, args, kwargs)[1]
+
+    # dynamically create a new class that implements the protocol
     connector = type(
-        f"{protocol.__name__}_sub",
+        f"{protocol.__name__}_connector",
         (protocol,),
-        {f: partial(func, f) for f in funcs} | {"_client": client},
+        {f: partial(call_remote, f) for f in api_funcs}
+        | {"_client": client, "_adapter_process": adapter_process},
     )
     instance = connector()
     return instance
