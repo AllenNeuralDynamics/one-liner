5581a71ef1f50560f5d1e4bb80b1e25fbdaaa13b
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2026-06-01 15:25:41 -0700

    port docstrings to numpydocs

--- src/one_liner/client.py
+++ src/one_liner/client.py
@@ -15,32 +15,40 @@ class RouterClient:
 
     def __init__(self, protocol: Protocol = "tcp", interface: str = "localhost",
                  rpc_port: str = "5555", broadcast_port: str = "5556",
-                 context: zmq.Context = None):
-        """
-        Create and return a `RouterClient` instance and connect it to an
+                 context: zmq.Context | None = None):
+        """ Create and return a `RouterClient` instance and connect it to an
         existing `RouterServer`.
 
-        :param protocol: a valid zmq protocol (``"tcp"``, ``"inproc"``, etc.)
-        :param interface: protocol interface. For `tcp`, this is the ip address
-           of the PC running the :py:class:`~one_liner.server.RouterServer`.
-           Default is ``"localhost"``.
-        :param rpc_port: port to issue remote function calls to python object
-           instances on the corresponding :py:class`~one_liner.server.RouterServer`.
-        :param broadcast_port: port to receive streamed messages from the
-           corresponding :py:class:`~one_liner.server.RouterServer`'s stream
-           client.
-        :param context: The zmq Context. If unspecified, this class instance
-           will take the default global zmq context for this process.
+        Parameters
+        ----------
+        protocol:
+            a valid zmq protocol (`"tcp"`, `"inproc"`, etc.)
+        interface:
+            protocol interface. For `"tcp"`, this is the ip address
+            of the PC running the
+            [`RouterServer`][one_liner.server.RouterServer].
+        rpc_port:
+            port to issue remote function calls to python object
+            instances on the corresponding
+            [`RouterServer`][one_liner.server.RouterServer].
+        broadcast_port:
+            port to receive streamed messages from the
+            corresponding [`RouterServer`][one_liner.server.RouterServer]'s
+            stream client.
+        context:
+            The zmq Context. If unspecified, this class instance
+            will take the default global zmq context for this process.
 
-        .. warning::
-           ``rpc_port`` and ``broadcast_port`` values must match those of the
-           :py:class:`~one_liner.server.RouterServer` that this ``RouterClient``
-           is connecting to.
+        Warnings
+        --------
+        `rpc_port` and `broadcast_port` values must match those of the
+        [`RouterServer`][one_liner.server.RouterServer] that this `RouterClient`
+        is connecting to.
 
-        .. note::
-           For the `protocol` setting, some options are system-dependent (i.e:
-           `ipc` is for unix-like OSes only).
-
+        Notes
+        -----
+        For the `protocol` setting, some options are system-dependent (i.e:
+        `"ipc"` is for unix-like OSes only).
         """
         self._log = logging.getLogger(self.__class__.__name__)
         self._context = context or zmq.Context.instance()
@@ -56,23 +64,36 @@ class RouterClient:
                      get_timestamp: bool = False,
                      deserializer: Encoding | Callable = "pickle") \
             -> Any | Tuple[float, Any]:
-        """Lookup a configured call by its `'call_name'`, call it with the
-        specified args/kwargs, and return the result.
 
-        :pararm call_name: the name of the call specified in
-            :py:meth:`~one_liner.server.ZMQRPCServer.add_named_call`.
-        :param args: args to pass to the underlying function. Note that any arg
-            passed in will overwrite any existing pre-configured arg setup in
-            :py:meth:`~one_liner.server.ZMQRPCServer.add_named_call`.
-        :param kwargs: kwargs to pass to the underlying function. Note that
-            these kwargs will overwrite existing pre-configured args setup with
-            :py:meth:`~one_liner.server.ZMQRPCServer.add_named_call` via
-            a standard dict update.
-        :param get_timestamp: if specified, return the data as a tuple
-            where the first value is a server-specified timestamp.
-        :param deserializer: callable function to deserialize the data or
-            string-representation of one of the built-in options.
+        """Lookup a configured call by its `call_name`, call it with the specified
+        args/kwargs, and return the result.
 
+        Parameters
+        ----------
+        call_name :
+            The name of the call specified in
+            [`add_named_call`][one_liner.server.ZMQRPCServer.add_named_call].
+        args :
+            Args to pass to the underlying function. Note that any arg passed in
+            will overwrite any existing pre-configured arg setup in
+            [`add_named_call`][one_liner.server.ZMQRPCServer.add_named_call].
+        kwargs :
+            Kwargs to pass to the underlying function. Note that these kwargs
+            will overwrite existing pre-configured args setup with
+            [`add_named_call`][one_liner.server.ZMQRPCServer.add_named_call]
+            via a standard dict update.
+        get_timestamp :
+            If specified, return the data as a tuple where the first value is a
+            server-specified timestamp.
+        deserializer :
+            Callable function to deserialize the data or string-representation
+            of one of the built-in options.
+
+        Returns
+        -------
+        object or tuple
+            The result of the call. If `get_timestamp` is true, returns a tuple
+            containing (server_timestamp, result).
         """
         return self.rpc_client.call_by_name(call_name=call_name, args=args,
                                             get_timestamp=get_timestamp,
@@ -83,21 +104,34 @@ class RouterClient:
              deserializer: Encoding | Callable = "pickle") \
             -> Any | Tuple[float, Any]:
         """Call a function/method within the scope of the connected
-        :py:class:`~one_liner.server.RouterServer` and return the result.
+        [`RouterServer`][one_liner.server.RouterServer] and return the result.
 
-        :param obj_name: object name. (Class instance or module)
-        :param attr_name: a callable attribute
-        :param args: list of positional arguments for function call
-        :param kwargs: dict of keyword arguments for function call
-        :param get_timestamp: if specified, return the data as a tuple
+        Parameters
+        ----------
+        obj_name:
+            object name. (Class instance or module)
+        attr_name:
+            a callable attribute
+        args:
+            list of positional arguments for function call
+        kwargs:
+            dict of keyword arguments for function call
+        get_timestamp:
+            if specified, return the data as a tuple
             where the first value is a server-specified timestamp.
-        :param deserializer: callable function to deserialize the data or
+        deserializer:
+            callable function to deserialize the data or
             string-representation of one of the built-in options.
-        :raises RPCException: if the underlying function call raises an exception
 
-        .. note::
-           This is a blocking call that returns after the response has been
-           returned.
+        Raises
+        ------
+        RPCException
+            if the underlying function call raises an exception
+
+        Notes
+        -----
+        This is a blocking call that returns after the response has been
+        returned.
 
         """
         return self.rpc_client.call(obj_name, attr_name, args, kwargs,
@@ -108,17 +142,23 @@ class RouterClient:
                          storage_type: Literal["queue", "cache"] = "queue",
                          deserializer: Encoding | Callable = "pickle"):
         """Configure data received from a stream to either hold one the latest
-        data (`cache`) or to hold onto all data in a buffer (`queue`) of
+        data (`"cache"`) or to hold onto all data in a buffer (`"queue"`) of
         size 1000.
 
-        :param name: stream name
-        :param storage_type: ``"cache"`` or ``"queue"``.
-           If ``"cache"``, calling :func:`get_stream` will return the most
-           recently received stream data and not buffer any incoming data.
-           The ``"queue"`` option will buffer up to 1000 messages such that
-           calling :func:`get_stream` will return data in a first-in-first-out
-           (FIFO) manner.
-        :param deserializer: callable function to deserialize the data or
+        Parameters
+        ----------
+        name:
+            stream name
+        storage_type:
+            ``"cache"`` or ``"queue"``. If `"cache"`, calling
+           [`get_stream`][one_liner.client.RouterClient.get_stream]
+           will return the most recently received stream data and not buffer any
+           incoming data.
+           The `"queue"` option will buffer up to 1000 messages such that
+           calling [`get_stream`][one_liner.client.RouterClient.get_stream] will
+           return data in a first-in-first-out (FIFO) manner.
+        :param deserializer:
+            callable function to deserialize the data or
             string-representation of one of the built-in options.
 
         """
@@ -126,52 +166,70 @@ class RouterClient:
 
     def get_stream(self, name: str, block: bool = False) -> Tuple[float, Any]:
         """Receive the results of a configured stream as 2-tuple where the first
-        value is a :py:class:`~one_liner.server.RouterServer`-specified
+        value is a [`RouterServer`][one_liner.server.RouterServer]-specified
         timestamp and the second value is the stream data..
 
-        :param name: stream name
-        :param block: if true, block until new data arrives.
-        :raises zmq.Again: if block is False (default) and no data is present.
-        :raises StreamException: if the connected
-           :py:class:`~one_liner.serverRouterServer`'s underlying function call
-           raised an exception.
+        Parameters
+        ----------
+        name:
+            stream name
+        block:
+            if true, block until new data arrives.
 
-        .. warning::
-           This stream must first be configured with :func:`configure_stream`.
+        Raises
+        ------
+        zmq.Again
+            if block is False (default) and no data is present.
+        StreamException
+            if the connected [`RouterServer`][one_liner.server.RouterServer]'s
+            underlying function call raised an exception.
+
+        Warnings
+        --------
+        This stream must first be configured on the `RouterClient`-side with
+        [`configure_stream`][one_liner.client.RouterClient.configure_stream].
         """
         return self.stream_client.get(name, block=block)
 
     def enable_stream(self, name: str):
         """Enable broadcasting of a stream by name. The connected
-        :py:class:`~one_liner.server.RouterServer` will start periodically calling
-        the underlying stream function, and calls to `get_stream(name)`
+        [`RouterServer`][one_liner.server.RouterServer] will start periodically
+        calling the underlying stream function, and calls to `get_stream(name)`
         will return new data.
 
-        :param name: stream name
+        Parameters
+        ----------
+        name:
+            stream name
 
-        .. note::
-           Enabling streams only works for periodically-added streams
-           added with :py:meth:`~one_liner.server.RouterServer.add_stream` and
-           :py:meth:`~one_liner.server.RouterServer.add_zmq_stream` but
-           *not* :py:meth:`~one_liner.server.RouterServer.get_stream_fn`.
-
+        Notes
+        -----
+        Enabling streams only works for periodically-added streams
+        added with [`add_stream`][one_liner.server.RouterServer.add_stream]
+        and [`add_zmq_stream`][one_liner.server.RouterServer.add_zmq_stream]
+        but *not* [`get_stream_fn`][one_liner.server.RouterServer.get_stream_fn].
         """
         # Use rpc_client to enable/disable streams.
         return self.rpc_client.call("__streamer", "enable", args=[name])
 
     def disable_stream(self, name: str):
         """Disable broadcasting of a stream by name. The connected
-        :py:class:`~one_liner.server.RouterServer` will stop periodically calling
-        the underlying stream function, and calls to `get_stream(name)`
-        will return no new data.
+        [`RouterServer`][one_liner.server.RouterServer] will stop periodically
+        calling the underlying stream function, and calls to
+        [`get_stream(name)`][one_liner.client.RouterClient.get_stream] will
+        return no new data.
 
-        :param name: stream name.
+        Parameters
+        ----------
+        name:
+            stream name.
 
-        .. note::
-           Disabling streams only works for periodically-added streams
-           added with :py:meth:`~one_liner.server.RouterServer.add_stream` and
-           :py:meth:`~one_liner.server.RouterServer.add_zmq_stream` but
-           *not* :py:meth:`~one_liner.server.RouterServer.get_stream_fn`.
+        Notes
+        -----
+           Disabling streams only works for periodically-added streams added
+           with [`add_stream`][one_liner.server.RouterServer.add_stream] and
+           [`add_zmq_stream`][one_liner.server.RouterServer.add_zmq_stream] but
+           *not* [`get_stream_fn`][one_liner.server.RouterServer.get_stream_fn].
 
         """
         # Use rpc_client to enable/disable streams.
@@ -180,9 +238,11 @@ class RouterClient:
     def get_stream_configurations(self, as_dict: bool = False) -> Streams | dict:
         """Get the configuration for all streams.
 
-        :param as_dict: if `True`, get the schema representation as a dict.
-           Otherwise, return a :py:class:`~one_liner.stream_schema.Streams`.
-
+        Parameters
+        ----------
+        as_dict:
+            if `True`, get the schema representation as a dict. Otherwise,
+            return a [`Streams`][one_liner.stream_schema.Streams] model.
         """
         return self.rpc_client.call("__streamer", "get_configuration",
                                     kwargs={"as_dict": as_dict})
@@ -198,7 +258,8 @@ class RouterClient:
         return self.rpc_client.call("__router_server", "get_version")
 
     def close(self):
-        """Close the connection to the :py:class:`~one_liner.server.RouterServer`."""
+        """Close the connection to the
+        [`RouterServer`][one_liner.server.RouterServer]"""
         self.stream_client.close()
         self.rpc_client.close()
 
@@ -228,7 +289,7 @@ class ZMQRPCClient:
              deserializer: Encoding | Callable = "pickle") \
         -> Any | Tuple[float, Any]:
         """Call a remote function available to the connected
-        :py:class:`~one_liner.server.RouterServer` and return the result.
+        [`RouterServer`][one_liner.server.RouterServer] and return the result.
 
         """
         args = [] if args is None else args
@@ -269,8 +330,14 @@ class ZMQStreamClient:
                          deserializer: Encoding | Callable = "pickle"):
         """Create a subscriber socket to receive a specific topic and setup
         how to buffer data.
-        * queue -> FIFO.
-        * cache -> only the latest data is received.
+
+        Parameters
+        ---------
+        name:
+            stream name.
+        storage_type:
+            * `"queue"` -> FIFO.
+            * `"cache"` -> only the latest data is received.
         """
         self.deserializers[name] = deserializer
         # Create zmq socket and configure to either queue or get-the-latest data.
@@ -289,9 +356,12 @@ class ZMQStreamClient:
     def get(self, stream_name: str, block: bool = False) -> Tuple[float, any]:
         """Return the timestamped data.
 
-        :raises zmq.Again: if block is `False` (default) and no data is present.
-        :raises StreamException: if the underlying function raised an exception
-           while being executed.
+        Raises
+        ------
+        `zmq.Again`
+            if block is `False` (default) and no data is present.
+        StreamException
+            if the underlying function raised an exception while being executed.
         """
         flag = 0 if block else zmq.NOBLOCK
         success, timestamp, *data = _recv(self.sub_sockets[stream_name], flag=flag,
